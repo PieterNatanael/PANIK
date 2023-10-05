3ad25f5947364c4906ec8453e0d0e3e9009779d0
PieterNatanael <pieternatanael@icloud.com>
2023-10-05 09:49:04 +0800

    Initial Commit

---
+++ PANIK/PANIKApp.swift
@@ -0,0 +1,17 @@
+//
+//  PANIKApp.swift
+//  PANIK
+//
+//  Created by Pieter Yoshua Natanael on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PANIKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
